e00408ad269e3bfb2bd119abfb2f5bad70cf6f4b
Khahory <angeld05@live.com>
2021-08-19 23:49:53 +0200

    Renderizado del lado del servidor
    (la diferencia es que el JSX se muestra en el codigo fuente de la pagina y en el cliente no)

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -2,14 +2,42 @@ import { useRouter } from "next/router";
 import Title from "../../components/Tittle";
 import Layout from "../../components/Layout";
 
-export default function Id(){
+export default function Id({post}){
     const router = useRouter()
 
     return(
         <Layout>
             <Title>Post ID</Title>
-            <h2>Detalles del post</h2>
-            <p>ID del post: {router.query.id}</p>
+            <div className='card'>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            <style jsx>
+                {`
+                  .card {
+                    margin: 1rem;
+                    flex-basis: 45%;
+                    padding: 1.5rem;
+                    color: black;
+                    text-decoration: none;
+                    border: 2px solid #eaeaea;
+                    border-radius: 10px;
+                    transition: color 0.15s ease, border-color 0.15s ease;
+                  }
+                `}
+            </style>
         </Layout>
     )
+}
+
+//RENDERIZADO DEL LADO DEL SERVIDOR
+// (la diferencia es que el JSX se muestra en el codigo fuente de la pagina y en el cliente no)
+export async function getServerSideProps(context){
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const post = await res.json()
+    return {
+        props: {
+            post
+        }
+    }
 }
--- pages/post/index.js
+++ pages/post/index.js
@@ -4,29 +4,30 @@ import Layout from "../../components/Layout";
 import React from "react";
 import Link from "next/link";
 
-export default function Index(){
-    const [posts, setPosts] = React.useState([])
-
-    // Esto carga despues de cargar este componente
-    React.useState(() => {
-        // Llamada api
-        const fetchPosts = async () => {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-            const newPosts = await res.json()
-            setPosts(newPosts)
-        }
-
-        fetchPosts().then(r => console.log('done api posts'))
-    }, [])
+export default function Post({posts}){
+    //RENDERIZADO DESDE EL CLIENTE
+    // const [posts, setPosts] = React.useState([])
+    //
+    // // Esto carga despues de cargar este componente
+    // React.useState(() => {
+    //     // Llamada api
+    //     const fetchPosts = async () => {
+    //         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    //         const newPosts = await res.json()
+    //         setPosts(newPosts)
+    //     }
+    //
+    //     fetchPosts().then(r => console.log('done api posts'))
+    // }, [])
 
     return(
         <Layout>
             <Title>Publicaciones</Title>
-            <div>
+            <div className={'grid'}>
                 {posts.map(post => {
                     return (
                         <Link href={`/post/[id]`} as={`/post/${post.id}`} key={post.id}>
-                            <a>
+                            <a className={'card'}>
                                 <h3>{post.title}</h3>
                                 <p>{post.body}</p>
                             </a>
@@ -34,6 +35,57 @@ export default function Index(){
                     )
                 })}
             </div>
+            <style jsx>
+                {`
+                  .grid {
+                    display: flex;
+                    flex-wrap: wrap;
+                    max-width: 800px;
+                    margin-top: 2rem;
+                  }
+
+                  .card {
+                    margin: 1rem;
+                    flex-basis: 45%;
+                    padding: 1.5rem;
+                    color: black;
+                    text-decoration: none;
+                    border: 2px solid #eaeaea;
+                    border-radius: 10px;
+                    transition: color 0.15s ease, border-color 0.15s ease;
+                  }
+
+                  .card:hover,
+                  .card:focus,
+                  .card:active {
+                    color: #0070f3;
+                    border-color: #0070f3;
+                  }
+
+                  .card h3 {
+                    margin: 0 0 1rem 0;
+                    font-size: 1.5rem;
+                  }
+
+                  .card p {
+                    margin: 0;
+                    font-size: 1.25rem;
+                    line-height: 1.5;
+                  }
+                `}
+            </style>
         </Layout>
     )
+}
+
+//RENDERIZADO DEL LADO DEL SERVIDOR
+// (la diferencia es que el JSX se muestra en el codigo fuente de la pagina y en el cliente no)
+export async function getServerSideProps(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await res.json()
+    return {
+        props: {
+            posts
+        }
+    }
 }
