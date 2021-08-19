cbb3b363d6dfd7eeb27dec59b37bac1c69b2d120
Khahory <angeld05@live.com>
2021-08-19 21:53:36 +0200

    navbar

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
 
 export default function Id(){
     const router = useRouter()
 
     return(
         <div>
+            <Navbar />
             <h2>Detalles del usuario</h2>
             <p>ID del usuario: {router.query.id}</p>
         </div>
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,6 +1,9 @@
+import Navbar from "../../components/Navbar";
+
 export default function Index(){
     return(
         <div>
+            <Navbar />
             Usuarios
         </div>
     )
