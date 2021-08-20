52488f60e3b8e79f594d05d4dd2571174ca370c0
Khahory <angeld05@live.com>
2021-08-20 21:04:11 +0200

    STATIC PATHS
     Funciona solo con los paths que hemos declarado:
         const paths = [
             { params: { id: '1' } },
             { params: { id: '2' } },
         ]

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -2,17 +2,71 @@ import { useRouter } from "next/router";
 
 import Title from "../../components/Tittle";
 import Layout from "../../components/Layout";
+import getRouteFromAssetPath from "next/dist/shared/lib/router/utils/get-route-from-asset-path";
 
-export default function Id(){
+export default function Usuario( {user} ){
     const router = useRouter()
+
+    if (router.isFallback)
+        return <div>CARGANDO...</div>
 
     return(
         <Layout>
             <Title>Usuario ID</Title>
-            <h2>Detalles del usuario</h2>
-            <p>ID del usuario: {router.query.id}</p>
+            <div className={'card'}>
+                <h3>Usuario</h3>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <p>Phone: {user.phone}</p>
+                <p>Website: {user.website}</p>
+            </div>
+            <style jsx>
+                {`
+                  .card {
+                    margin: 1rem;
+                    flex-basis: 45%;
+                    padding: 1.5rem;
+                    text-align: left;
+                    color: inherit;
+                    text-decoration: none;
+                    border: 1px solid #eaeaea;
+                    border-radius: 10px;
+                    transition: color 0.15s ease, border-color 0.15s ease;
+                  }
+
+                  .card:hover,
+                  .card:focus,
+                  .card:active {
+                    color: #0070f3;
+                    border-color: #0070f3;
+                  }
+
+                  .card h3 {
+                    margin: 0 0 1rem 0;
+                    font-size: 1.5rem;
+                  }
+
+                  .card p {
+                    margin: 0;
+                    font-size: 1.25rem;
+                    line-height: 1.5;
+                  }
+                `}
+            </style>
         </Layout>
     )
+}
+
+export async function getStaticPaths(){
+    const paths = [
+        { params: { id: '1' } },
+        { params: { id: '2' } },
+    ]
+
+    return {
+        paths,
+        fallback: false
+    }
 }
 
 export async function getStaticProps({ params }) {
