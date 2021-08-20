6af9952c00342bc89daee740aa71ff0868d66d64
Khahory <angeld05@live.com>
2021-08-20 21:08:26 +0200

    STATIC PATHS
    creamos un path para cada usuario

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -58,10 +58,19 @@ export default function Usuario( {user} ){
 }
 
 export async function getStaticPaths(){
-    const paths = [
-        { params: { id: '1' } },
-        { params: { id: '2' } },
-    ]
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const users = await res.json();
+
+    // const paths = [
+    //     { params: { id: '1' } },
+    //     { params: { id: '2' } },
+    // ]
+
+    const paths = users.map(user => {
+        return {
+            params: { id: `${user.id}` }
+        }
+    });
 
     return {
         paths,
